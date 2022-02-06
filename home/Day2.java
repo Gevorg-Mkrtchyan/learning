bed016027b28b8a84a9c09d816e5cb8078654220
Gevorg-Mkrtchyan <gevmkrtchyan33@gmail.com>
2022-02-06 19:44:16 +0400

    learning

---
+++ home/Day2.java
@@ -0,0 +1,138 @@
+package homework.home;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Day2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        // DAY2 homework
+        // task 1
+//    public static void main(String[] args) {
+//
+//    }
+
+        // task 2
+//        int min = scanner.nextInt();
+//        int hour = min / 60;
+//        int minutes = min % 60;
+//
+//        System.out.println(hour + " : " + minutes);
+
+        // task 3
+
+//        int num = scanner.nextInt();
+//        if (num % 2 == 0) {
+//            for (int i = 0; i < num; i = i + 2) {
+//                System.out.println("some of even in :" + i);
+//            }
+//        } else {
+//            for (int i = 1; i < num; i = i + 2) {
+//                System.out.println("some of even in :" + i);
+//            }
+//        }
+
+        //task 4
+//        int num1 = scanner.nextInt();
+//        int num2 = scanner.nextInt();
+//        System.out.println(num1 * num2);
+
+        //task 5
+//        int a =scanner.nextInt();
+//        int b = scanner.nextInt();
+//        int c = scanner.nextInt();
+//        int result = (a + b + c) / 3;
+//        System.out.println(result );
+
+        // task 6
+
+//        // right-angled triangle
+//        int angled1 = scanner.nextInt();
+//        int angled2 = scanner.nextInt();
+//        int result = angled1 * angled2 / 2;
+//        System.out.println("right-angled triangle area is :" + result);
+//
+//        //square
+//        int square = scanner.nextInt();
+//        System.out.println("square area is :" + Math.pow(square, 2));
+//
+//        // rectangle
+//
+//        int rect1 = scanner.nextInt();
+//        int rect2 = scanner.nextInt();
+//        System.out.println( "rectangle area is :" + rect1 * rect2);
+
+        // task 7
+//        System.out.println("please enter number");
+//        byte b = scanner.nextByte();
+//        short c = scanner.nextShort();
+//        int a = scanner.nextInt();
+//        int sum = (a + b + c) / 3;
+//        System.out.println("average of them " + sum);
+
+        // task8
+//        System.out.println("please enter 10 or 15");
+//        int n = scanner.nextInt();
+//        int c = 12;
+//        int s = 3 * n * n - c * n;
+//        System.out.println(s);// 180,495
+
+        // task 9
+
+//        System.out.println("please enter number");
+//        int n = scanner.nextInt();
+//        System.out.println(n % 10);
+
+        // task 11
+
+//        System.out.println("please enter number");
+//        int a = scanner.nextInt();
+//        int b = scanner.nextInt();
+//        System.out.println(a % b);
+
+        // task 12
+
+//      int rand = random.nextInt();
+//        System.out.println(rand);
+
+        // task 13
+
+//        int rando = random.nextInt(16);
+//        System.out.println(rando);
+
+        // task 14
+
+//        int ran = random.nextInt(16) + 15;
+//        System.out.println(ran);
+
+        // task 15
+
+//        int odd = scanner.nextInt();
+//        if (odd % 2 == 1) {
+//            System.out.println("odd");
+//        } else {
+//            System.out.println("even");
+//        }
+//        boolean isPrime = odd % 2 == 1;
+//        System.out.println(isPrime);
+
+        //task 16,17,18
+//        int firstNum = scanner.nextInt();
+//        int secondNum = scanner.nextInt();
+//        int thirdNum = scanner.nextInt();
+//        System.out.println("second digit is :" + secondNum / 10 % 10); // task 16
+//        System.out.println("first digit is :" + firstNum / 100);//task 17
+//        int result = thirdNum % 10 + secondNum / 10 % 10 + firstNum / 100;
+//        System.out.println("sum of all 3 digits : " + result); // 18
+
+        //task 19
+//        int a = scanner.nextInt();
+//        int b = scanner.nextInt();
+//        int c = a;
+//        a = b;
+//        b = c;
+//        System.out.println(a + " : " + b);
+
+    }
+}
