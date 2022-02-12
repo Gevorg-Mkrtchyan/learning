571ce8f406598412efe1277302247d7084c5c926
Gevorg-Mkrtchyan <gevmkrtchyan33@gmail.com>
2022-02-12 20:30:31 +0400

    commit

--- src/main/java/home/company/day5/task1/PersonTest.java
+++ src/main/java/home/company/day5/task1/PersonTest.java
@@ -1,10 +1,10 @@
 package home.company.day5.task1;
 
 public class PersonTest {
-//    public static void main(String[] args) {
+    public static void main(String[] args) {
 //        Person person1 = new Person();
 //        Person person2 = new Person("Jone", 21, "Erevan");
 //        person1.allInfoPerson();
 //        person2.allInfoPerson();
-//    }
+    }
 }
