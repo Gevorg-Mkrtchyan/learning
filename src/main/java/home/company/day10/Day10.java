045889e2d3e0ca7a414aeb7303f17a018e29a056
Gevorg-Mkrtchyan <gevmkrtchyan33@gmail.com>
2022-02-14 17:50:51 +0400

    commit

--- src/main/java/home/company/day10/Day10.java
+++ src/main/java/home/company/day10/Day10.java
@@ -1,4 +1,177 @@
 package home.company.day10;
 
+import java.util.Scanner;
+
 public class Day10 {
+    Scanner scanner = new Scanner(System.in);
+    /**
+     * 1 .Print all integer numbers from 1 to 10.
+     */
+//    public void allInt() {
+//        int num = 0;
+//        while (num < 10) {
+//            num++;
+//            System.out.println(num);
+//        }
+//    }
+
+    /**
+     * 2.Print all even integer numbers from 1 to 20.
+     */
+//    public void evenNum() {
+//        int num = 0;
+//        while (num < 20) {
+//            num += 2;
+//            System.out.println(num);
+//        }
+//    }
+
+    /**
+     * 3.Print all exact squares of natural numbers that are not
+     * exceeding a given positive integer N. Input N from console.
+     * Example`
+     * Input number - 15
+     * Output `
+     * 1
+     * 4
+     * 9
+     * @param n
+     */
+//    public void squares(int n) {
+//        n = scanner.nextInt();
+//        int num = 1;
+//        while (num * num < n) {
+//            System.out.println(num * num);
+//            num++;
+//        }
+//    }
+
+    /**
+     * 4. Given an integer number, greater than 2.Find and print the
+     * smallest natural divisor other than 1 for a given number.Input
+     * number from console.Example `
+     * Input` 15
+     * Output ` 3
+     * @param n
+     */
+//    public void divisorNumber(int n) {
+//        n = scanner.nextInt();
+//        int i = 2;
+//        while (n % i != 0) {
+//            i++;
+//            System.out.println(i);
+//        }
+//    }
+
+    /**
+     * 5.Print all integer powers of two not exceeding N in ascending
+     * order.Input N from console. Example ` Input 50(You can not use
+     * Math.pow())
+     * Output ` 1 2 4 8 16 32
+     * @param num
+     */
+//    public void divisor2(int num) {
+//        num = scanner.nextInt();
+//        int m = 1;
+//        while (m * 2 < num) {
+//            m *= 2;
+//            System.out.println(m);
+//        }
+//    }
+    /**
+     *6.For a given integer number N,print all integers in descending
+     * order.
+     * @param number1
+     */
+//    public void descendingNumber(int number1){
+//        System.out.println("enter number1");
+//        number1 =scanner.nextInt();
+//        System.out.println("enter number2");
+//        int number2 = scanner.nextInt();
+//        while (number1 >= number2){
+//            number1--;
+//            System.out.println(number1);
+//        }
+//    }
+
+    /**
+     * 7.For a given integer number N,print all integers that are multiple of
+     * 2 in descending order.
+     *
+     * @param number
+     */
+//    public void allMultiple(int number) {
+//        System.out.println("enter number");
+//        number = scanner.nextInt();
+//        if (number%2 == 0){
+//            while (number >= 2){
+//                number-=2;
+//                System.out.println(number);
+//            }
+//        }
+//    }
+
+    /**
+     * 8. Input N natural number till N would be the exact power of 2.If N is
+     * the exact power of 2 .
+     * - Print N
+     * - Print count of input integers
+     * - break loop
+     * @param a
+     */
+//    public boolean square(int a) {
+//        int num = a;
+//        while (num % 2 == 0 && num > 0) {
+//            num /= 2;
+//        }
+//        return num == 1;
+//    }
+//    public void calculatorSquare() {
+//        int z;
+//        int count = 0;
+//        while (true) {
+//            z = scanner.nextInt();
+//            count++;
+//            if (square(z)) {
+//                break;
+//            }
+//        }
+//        System.out.println(z + " : " + count);
+//    }
+
+    /**
+     * 9.For a given natural N print the smallest integer k that`
+     * Math.pow(2,k)≥N.(You can not use Math.pow())
+     * Example` Input 7
+     * Output 3
+     */
+//    public void smallPow(int n) {
+//        n = scanner.nextInt();
+//        int k = 1;
+//        int count = 0;
+//        while (k <= n) {
+//            if (k == n) {
+//                System.out.println(n);
+//                break;
+//            } else {
+//                k *= 2;
+//                count++;
+//            }
+//        }
+//        System.out.println(k + " - " + count);
+//    }
+    /**
+     *10.Write Java program to print the table of characters that are
+     * equivalent to the Ascii codes from 32 to 122.Print 10 characters
+     * at each line.
+     */
+//    public void charEvoluation(){
+//        int start = 32;
+//        int end = 122;
+//        while (start <= end){
+//
+//        }
+//    }
+
 }
+
--- src/main/java/home/company/day10/TestDay10.java
+++ src/main/java/home/company/day10/TestDay10.java
@@ -1,4 +1,17 @@
 package home.company.day10;
 
 public class TestDay10 {
+    public static void main(String[] args) {
+        Day10 day10 = new Day10();
+//        day10.allInt(); // task1
+//        day10.evenNum(); //task2
+//        day10.squares(10); //task3
+//        day10.divisorNumber(2); //task4
+//        day10.divisor2(5); // task 5
+//        day10.descendingNumber(6); //task 6
+//        day10.allMultiple(5); //task 7
+//        day10.calculatorSquare(); //task8
+//        day10.smallPow(5); // task 9
+
+    }
 }
