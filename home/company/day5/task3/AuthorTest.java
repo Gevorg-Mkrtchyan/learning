91f8d10b1df2ff637f621671d14096e79b794656
Gevorg-Mkrtchyan <78731439+Gevorg-Mkrtchyan@users.noreply.github.com>
2022-02-14 13:44:13 +0400

    Delete home/company directory

--- home/company/day5/task3/AuthorTest.java
+++
@@ -1,8 +0,0 @@
-package home.company.day5.task3;
-
-public class AuthorTest {
-    public static void main(String[] args) {
-        Author author = new Author("Jhon", "book.@gmail.com", 'm');
-        author.allInfoAuthor();
-    }
-}
