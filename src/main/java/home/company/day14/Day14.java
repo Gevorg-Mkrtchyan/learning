3bc22ac5f7d6576f6e55d5643cd7dee783d86ac3
Gevorg-Mkrtchyan <gevmkrtchyan33@gmail.com>
2022-02-22 13:26:19 +0400

    commit

--- src/main/java/home/company/day14/Day14.java
+++ src/main/java/home/company/day14/Day14.java
@@ -247,8 +247,9 @@ public class Day14 {
     public int charCount(String str, char ch) {
         int count = 0;
         for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == ch)
+            if (str.charAt(i) == ch) {
                 count++;
+            }
         }
         return count;
     }
