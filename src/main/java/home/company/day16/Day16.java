a4f6def0955c09be966258b14f20d0efbd5f4c28
Gevorg-Mkrtchyan <gevmkrtchyan33@gmail.com>
2022-02-23 22:09:14 +0400

    commit

---
+++ src/main/java/home/company/day16/Day16.java
@@ -0,0 +1,201 @@
+package home.company.day16;
+
+import java.util.Locale;
+
+public class Day16 {
+    /**
+     * 1 A natural number N is given (entered from the keyboard). Calculate two to the power of N
+     * Display the calculated value (1 <= N <= 15).
+     */
+    public int naturalNumber(int n) {
+        if (1 <= n && n <= 15) {
+            n = (int) Math.pow(2, n);
+            return n;
+        }
+        return -1;
+    }
+
+    /**
+     * 4.Given number n. N minutes have passed since the beginning of the day. Determine how many hours and minutes the
+     * digital clock will show at this moment. The program should print two numbers: the number of hours (from 0 to 23)
+     * and the number of minutes (from 0 to 59). Note that the number n can be more than the number of minutes in a day.
+     * Example` 150      2 30
+     * 1441   0   1
+     */
+    public void dayTime(int n) {
+        int hour;
+        int min;
+        if (n > 59) {
+            hour = n / 60;
+            if (hour > 23) {
+                hour -= 24;
+            }
+            min = n % 60;
+            System.out.println(hour + " : " + min);
+        }
+    }
+    /**
+     *  3 How many times will the body of the loop be executed?
+     * -        for (int i = 2; i <= 15; i ++)
+     *      {...}  // 14 times
+     * -        for (int i = 10; i <= 100; i = i+7)
+     * {...} // 13 tames
+     * -       for (float i = 1.5; i <= 10.3; i = i+0.4) //compile error
+     * {...}
+     */
+
+    /**
+     * 4.Write a java program to determine whether the number is prime or not.
+     */
+    public boolean prime(int number) {
+        if (number > 0 && number % 2 == 0) {
+            return false;
+        }
+        for (int i = 3; i < number / 2; i += 2)
+            if (number % i == 0) {
+                return false;
+            }
+        return true;
+    }
+
+    /**
+     * 5.You are given two four-digit numbers A and B. Print all four-digit numbers on the segment from A to B,
+     * the record of which is a palindrome.
+     * Example` 1600                1661
+     * 2100               1771
+     * 1881
+     * 1991
+     * 2002
+     */
+    public void palindromeNumber(int a, int b) {
+        while (a <= b) {
+            if (a / 1000 == a % 10 && a / 100 % 10 == a / 10 % 10) {
+                System.out.println(a);
+            }
+            a++;
+        }
+    }
+
+    /**
+     * 7. A three-letter word is given. The word consists only of Latin letters, small and large. Print the same word,
+     * where the first letter is capitalized, the rest are small.
+     * <p>
+     * Example` dog    Dog
+     * CAT             Cat
+     * Lip               Lip
+     */
+    public String upString(String str) {
+        return str.substring(0, 1).toUpperCase(Locale.ROOT) + str.substring(1).toLowerCase(Locale.ROOT);
+    }
+
+    /**
+     * 8.Enter the number N and draw an NxN checkerboard where the top left is white. Designate white margins
+     * O, black margins X. Use a for loop.
+     * <p>
+     * Input ` 3
+     * Output`
+     * OXO
+     * XOX
+     * OXO
+     * <p>
+     * <p>
+     * Input` 8
+     * <p>
+     * OXOXOXOX
+     * XOXOXOXO
+     * OXOXOXOX
+     * XOXOXOXO
+     * OXOXOXOX
+     * XOXOXOXO
+     * OXOXOXOX
+     * XOXOXOXO
+     */
+    public void printXOr0(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i % 2 == 0) {
+                    if (j % 2 != 0) {
+                        System.out.print("X");
+                    } else {
+                        System.out.print("0");
+                    }
+                } else {
+                    if (j % 2 != 0) {
+                        System.out.print("0");
+                    } else {
+                        System.out.print("X");
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * 9.The first term and the denominator of the geometric progression are given (real numbers b1 and q, q! = 0).
+     * An integer n is also given. Print the n-th term of a geometric progression. Don't use the pow function,
+     * use a for loop. Print the answer with precision exactly two characters after the period.
+     * <p>
+     * Example` Input ` 2   2           32.0
+     * 5
+     * 3.2   1.5          10.80
+     * 4
+     */
+    public double geometric(double number, double multipl, int step) {
+        for (int i = 0; i < step - 1; i++) {
+            number *= multipl;
+        }
+        return number;
+    }
+
+    /**
+     * 10.A natural number> = 2 is specified. Expand it into prime factors.
+     * Example` Input` 120            2*2*2*3*5
+     */
+    public void primeFactors(int number) {
+        for (int i = 2; i < number; i++) {
+            while (number % i == 0) {
+                System.out.println(i + " ");
+                number = number / i;
+            }
+        }
+        if (number > 2) {
+            System.out.println(number);
+        }
+    }
+
+    /**
+     * *10.Two numbers n and m are given. Create a two-dimensional array A [n] [m], fill it with the multiplication
+     * table A [i] [j] = i * j and display it. In this case, you cannot use nested loops, the entire filling of the
+     * array must be done in one cycle.
+     * Example` Input` 3   3
+     * Output` 0 0 0
+     * 0 1 2
+     * 0 2 4
+     */
+    public void printArray(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("[");
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(" ");
+                System.out.print(arr[i][j]);
+            }
+            System.out.println(" ]");
+        }
+    }
+
+    public void arrayLogic(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                arr[i][j] = i * j;
+            }
+        }
+    }
+
+    public void arrayTask1(int n) {
+        int[][] arr = new int[n][n];
+        arrayLogic(arr);
+        printArray(arr);
+    }
+
+}
---
+++ src/main/java/home/company/day16/TestDay16.java
@@ -0,0 +1,16 @@
+package home.company.day16;
+
+public class TestDay16 {
+    public static void main(String[] args) {
+        Day16 day16 = new Day16();
+//        System.out.println(day16.naturalNumber(16));//task 1
+//        day16.dayTime(150); // task2
+//        System.out.println(day16.prime(25)); //task4
+//        day16.palindromeNumber(1600, 2100);//task5
+//        System.out.println(day16.upString("CAR"));//task6
+//        day16.printXOr0(3);//task7
+//        System.out.println(day16.geometric(3.2, 1.5, 4));//task8
+//        day16.primeFactors(130);//task 9
+//        day16.arrayTask1(3);//task10
+    }
+}
