8e56020d5e2c4284adc12c00b374951668ad8e79
Gevorg-Mkrtchyan <gevmkrtchyan33@gmail.com>
2022-02-24 22:24:18 +0400

    commit

--- src/main/java/home/company/day17/TestDay17.java
+++ src/main/java/home/company/day17/TestDay17.java
@@ -8,14 +8,14 @@ public class TestDay17 {
         int[] arr1 = {1, 2, 3};
         float[] arr2 = {1.1f, 1.2f, 1.3f};
         double[] arr3 = {2.1, 2.2, 2.3};
-//        System.out.println(day17.print(arr2)); //task 4
-//        System.out.println(day17.search(arr1));
-//        System.out.println(day17.search(arr1,1));
-//        System.out.println(day17.search(arr1,1,1)); // task 5
-//        System.out.println(day17.area(5,4));//task 6
-//        System.out.println(day17.max(5,9,9,10));//task 7
-//        System.out.println(day17.distance(1,2,3,4));
-//        day17.fahrenheitCelsius(1,1.1f);//task9
-//        System.out.println(day17.remainder(333,2));//task10
+        System.out.println(day17.print(arr2)); //task 4
+        System.out.println(day17.search(arr1));
+        System.out.println(day17.search(arr1,1));
+        System.out.println(day17.search(arr1,1,1)); // task 5
+        System.out.println(day17.area(5,4));//task 6
+        System.out.println(day17.max(5,9,9,10));//task 7
+        System.out.println(day17.distance(1,2,3,4));
+        day17.fahrenheitCelsius(1,1.1f);//task9
+        System.out.println(day17.remainder(333,2));//task10
     }
 }
