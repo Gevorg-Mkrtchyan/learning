d5f6b426d5a1f6866f1f62908ff5b004f139af41
Gevorg-Mkrtchyan <gevmkrtchyan33@gmail.com>
2022-03-10 00:28:01 +0400

    commit

--- src/main/java/home/company/day22/task2/DefaultStack.java
+++ src/main/java/home/company/day22/task2/DefaultStack.java
@@ -4,42 +4,40 @@ import java.util.Scanner;
 
 public class DefaultStack implements Stack {
     private static final int MAX_SIZE = 5;
-    private final int[] nums = new int[MAX_SIZE];
-    int index;
+    private static int[] nums = new int[MAX_SIZE];
+    private int index;
 
-    @Override
-    public void push(int val) throws StackIndexOutOfBoundsException {
-        try {
-            if (index >= MAX_SIZE) {
-                throw new StackIndexOutOfBoundsException("no");
-            }
-            index++;
-            nums[index - 1] = val;
-        } catch (StackIndexOutOfBoundsException e) {
-            e.printStackTrace();
-        }
-
+    public DefaultStack(final int[] nums) {
+        DefaultStack.nums = nums;
     }
 
     @Override
-    public  int pop() {
+    public void push(int val) throws StackIndexOutOfBoundsException {
+        if (index == MAX_SIZE) {
+            throw new StackIndexOutOfBoundsException("index out of bounds");
+        } else {
+            nums[index++] = val;
+        }
+    }
 
+    @Override
+    public int pop() {
         if (index == 0) {
             throw new EmptyStackException("empty");
-        } else {
-            index--;
         }
+        index--;
         return nums[index];
     }
 
     public static void main(String[] args) throws StackIndexOutOfBoundsException {
         Scanner scanner = new Scanner(System.in);
-        DefaultStack defaultStack = new DefaultStack();
-//        defaultStack.push(scanner.nextInt());
-//        defaultStack.push(scanner.nextInt());
-//        defaultStack.push(scanner.nextInt());
-//        defaultStack.push(scanner.nextInt());
-//        defaultStack.push(scanner.nextInt());
-//        System.out.println(defaultStack.pop());
+        DefaultStack defaultStack = new DefaultStack(new int[5]);
+        defaultStack.push(scanner.nextInt());
+        defaultStack.push(scanner.nextInt());
+        defaultStack.push(scanner.nextInt());
+        defaultStack.push(scanner.nextInt());
+        defaultStack.push(scanner.nextInt());
+        defaultStack.push(scanner.nextInt());
+        System.out.println(defaultStack.pop());
     }
 }
